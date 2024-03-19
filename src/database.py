7541679a362c22f22f5c70545a7e265a6ba6e2cd
earlinn <earlinndrow@gmail.com>
2024-03-19 10:56:11 +0500

    Create async DB engine

--- src/database.py
+++ src/database.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import URL, create_engine, text
@@ -5,8 +7,18 @@ from sqlalchemy import URL, create_engine, text
 from config import settings
 
 
-engine = create_engine(url=settings.DATABASE_URL_psycopg, echo=True)
+sync_engine = create_engine(url=settings.DATABASE_URL_psycopg, echo=False)
+async_engine = create_async_engine(url=settings.DATABASE_URL_asyncpg, echo=False)
 
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT VERSION()"))
-    print(f"{result=}")
+with sync_engine.connect() as conn:
+    result = conn.execute(text("SELECT 1,2,3 UNION SELECT 4,5,6"))
+    print(f"sync {result.all()=}")
+
+
+async def get_123():
+    async with async_engine.connect() as conn:
+        result = await conn.execute(text("SELECT 1,2,3 UNION SELECT 4,5,6"))
+        print(f"async {result.all()=}")
+
+
+asyncio.run(get_123())
