ca5457d3d51bdb14c54154cd7cafbd1d3a1ffe66
earlinn <earlinndrow@gmail.com>
2024-03-19 16:04:13 +0500

    Implement data sampling and data updating via orm

--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 import os
 import sys
 
-from queries.core import SyncCore
+# from queries.core import SyncCore
 from queries.orm import SyncORM
 
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
 
 SyncORM.create_tables()
+# SyncCore.create_tables()
+
 SyncORM.insert_workers()
-SyncCore.select_workers()
-SyncCore.update_worker()
+# SyncCore.insert_workers()
+
+# SyncCore.select_workers()
+# SyncCore.update_worker()
+
+SyncORM.select_workers()
+SyncORM.update_worker()
--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from database import Base, async_session_factory, sync_engine, sync_session_factory
 from models import WorkersORM
 
@@ -13,15 +15,34 @@ class SyncORM:
     @staticmethod
     def insert_workers():
         with sync_session_factory() as session:
-            worker_beaver = WorkersORM(username="Jack")
-            worker_wolf = WorkersORM(username="Michael")
-            session.add_all([worker_beaver, worker_wolf])
+            worker_jack = WorkersORM(username="Jack")
+            worker_michael = WorkersORM(username="Michael")
+            session.add_all([worker_jack, worker_michael])
+            session.flush()
+            session.commit()
+
+    @staticmethod
+    def select_workers():
+        with sync_session_factory() as session:
+            # worker_id = 1
+            # worker_jack = session.get(WorkersORM, worker_id)
+            query = select(WorkersORM)
+            result = session.execute(query)
+            workers = result.scalars().all()
+            print(f"{workers=}")
+
+    @staticmethod
+    def update_worker(worker_id: int = 2, new_username: str = "Micha"):
+        with sync_session_factory() as session:
+            worker_michael = session.get(WorkersORM, worker_id)
+            worker_michael.username = new_username
+            session.refresh(worker_michael)
             session.commit()
 
 
 async def async_insert_data():
     async with async_session_factory() as session:
-        worker_beaver = WorkersORM(username="Beaver")
-        worker_wolf = WorkersORM(username="Wolf")
-        session.add_all([worker_beaver, worker_wolf])
+        worker_jack = WorkersORM(username="Jack")
+        worker_michael = WorkersORM(username="Michael")
+        session.add_all([worker_jack, worker_michael])
         await session.commit()
