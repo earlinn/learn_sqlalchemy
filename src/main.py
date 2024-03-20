faf735582b8c6f38ba21dd416cf54e2c986a4e08
earlinn <earlinndrow@gmail.com>
2024-03-20 16:06:45 +0500

    Create AsyncORM (asynchronous SyncORM), asynchronously call its methods

--- src/main.py
+++ src/main.py
@@ -1,21 +1,30 @@
+import asyncio
 import os
 import sys
 
 # from queries.core import SyncCore
-from queries.orm import SyncORM
+from queries.orm import AsyncORM  # , SyncORM
 
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
 
-SyncORM.create_tables()
-# SyncCore.create_tables()
 
-SyncORM.insert_workers()
-# SyncCore.insert_workers()
+async def main():
+    # SyncCore.create_tables()
+    # SyncCore.insert_workers()
+    # SyncCore.select_workers()
+    # SyncCore.update_worker()
 
-# SyncCore.select_workers()
-# SyncCore.update_worker()
+    # SyncORM.create_tables()
+    # SyncORM.insert_workers()
+    # SyncORM.select_workers()
+    # SyncORM.update_worker()
+    # SyncORM.insert_cvs()
+    # SyncORM.select_cvs_avg_compendation()
 
-SyncORM.select_workers()
-SyncORM.update_worker()
-SyncORM.insert_cvs()
-SyncORM.select_cvs_avg_compendation()
+    await AsyncORM.create_tables()
+    await AsyncORM.insert_workers()
+    await AsyncORM.select_workers()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,6 +1,12 @@
 from sqlalchemy import Integer, and_, cast, func, select
 
-from database import Base, async_session_factory, sync_engine, sync_session_factory
+from database import (
+    Base,
+    async_engine,
+    async_session_factory,
+    sync_engine,
+    sync_session_factory,
+)
 from models import CVORM, WorkersORM, WorkLoad
 
 
@@ -100,9 +106,26 @@ class SyncORM:
             print(result[0].avg_compensation)
 
 
-async def async_insert_data():
-    async with async_session_factory() as session:
-        worker_jack = WorkersORM(username="Jack")
-        worker_michael = WorkersORM(username="Michael")
-        session.add_all([worker_jack, worker_michael])
-        await session.commit()
+class AsyncORM:
+    @staticmethod
+    async def create_tables():
+        async with async_engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(Base.metadata.create_all)
+
+    @staticmethod
+    async def insert_workers():
+        async with async_session_factory() as session:
+            worker_jack = WorkersORM(username="Jack")
+            worker_michael = WorkersORM(username="Michael")
+            session.add_all([worker_jack, worker_michael])
+            await session.flush()
+            await session.commit()
+
+    @staticmethod
+    async def select_workers():
+        async with async_session_factory() as session:
+            query = select(WorkersORM)
+            result = await session.execute(query)
+            workers = result.scalars().all()
+            print(f"{workers=}")
